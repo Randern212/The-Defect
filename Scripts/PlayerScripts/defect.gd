9467139236b291a1f25c65536a903793c3730b63
randern212 <randern212@gmail.com>
2025-11-15 13:36:40 +0200

    added camera motion to the player

---
+++ Scripts/PlayerScripts/defect.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+
+@onready var stateMachine = $stateManager
+@onready var camera: pivotedCamera = $pivotedCamera
+
+func _ready()->void:
+	stateMachine.init(self)
+
+func _input(event: InputEvent) -> void:
+	camera.cameraMouseInput(event)
+	stateMachine.input(event)
+
+func _physics_process(delta: float) -> void:
+	camera.handleCameraMotionProcessing(delta)
+	stateMachine.physics_process(delta)
+	move_and_slide()
--- Scripts/Templates/pivotedCamera.gd
+++ Scripts/Templates/pivotedCamera.gd
@@ -1,9 +1,21 @@
 class_name pivotedCamera
 extends Node3D
 
-@export var camera: Camera3D
+var cameraInputDirection:Vector2=Vector2.ZERO
 
+@export var camera: Camera3D
 @export var cameraDistance:float=80
+@export_range(0.0, 1.0) var mouseSensitiviy:float=0.25
 
 func setCameraDistance(distance:float)->void:
 	self.camera.position.z=cameraDistance*-1
+
+func cameraMouseInput(event:InputEvent):
+	var isMoving:bool=(event is InputEventMouseMotion and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED)
+	if isMoving:
+		cameraInputDirection=event.screen_relative*mouseSensitiviy
+
+func handleCameraMotionProcessing(delta:float)->void:
+	self.rotation.x+=clamp(cameraInputDirection.y*delta,-PI/6,PI/3)
+	self.rotation.y+=clamp(cameraInputDirection.x*delta,-PI/6,PI/3)
+	cameraInputDirection=Vector2.ZERO
