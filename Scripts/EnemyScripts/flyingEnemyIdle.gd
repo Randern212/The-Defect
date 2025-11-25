505e85564d7a2b4124d8a659393d518fe424edba
randern212 <randern212@gmail.com>
2025-11-25 13:25:49 +0200

    finalized the Idle state for the flying enemy

--- Scripts/EnemyScripts/flyingEnemyIdle.gd
+++ Scripts/EnemyScripts/flyingEnemyIdle.gd
@@ -6,3 +6,9 @@ func _ready() -> void:
 
 func enter():
 	player.animations.play("IdleHovering")
+	player.velocity=Vector3.ZERO
+
+func physics_process(delta: float)->int:
+	if player.target:
+		return Constants.enemyStates.CHASE
+	return ID
