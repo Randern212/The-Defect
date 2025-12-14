164afbd3bb904f61c1dfd68919f22709d2f0ab86
randern212 <randern212@gmail.com>
2025-12-14 06:10:10 +0200

    maded the attack animation go back and forth to add variety to the animations

--- Scripts/PlayerScripts/defectAttack.gd
+++ Scripts/PlayerScripts/defectAttack.gd
@@ -1,17 +1,21 @@
 extends State
 
-
+var leftToRight:bool=true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.ID=Constants.playerStates.ATTACK
 
 func enter()->void:
-	player.body.animations.play("Slash")
+	if leftToRight:
+		player.body.animations.play("Slash")
+	else:
+		player.body.animations.play_backwards("Slash")
 	turnHitbox(true)
 
 
 func exit()->void:
 	turnHitbox(false)
+	leftToRight = not leftToRight
 
 
 func turnHitbox(On:bool)->void:
